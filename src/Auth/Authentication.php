46f7820f9ec2afadb28a71ec4423456285f05c1b
Vinícius Barros <vmbarross@gmail.com>
2017-08-09 22:10:09 +0100

    Showing options on top bar according to authentication status

--- src/Auth/Authentication.php
+++ src/Auth/Authentication.php
@@ -19,6 +19,13 @@ class Authentication
     protected $authenticated = false;
     protected $errors        = [];
 
+    /**
+     * Tries to perform an user authentication
+     * 
+     * @param type $email
+     * @param type $password
+     * @return $this
+     */
     public function doAuthentication($email, $password)
     {
         $user = User::where('email', $email)->first();
@@ -40,6 +47,7 @@ class Authentication
 
     /**
      * Checks weather the user is authenticated
+     * 
      * @return boolean
      */
     public function isAuthenticated()
@@ -52,26 +60,48 @@ class Authentication
         return $this->authenticated;
     }
 
-    private function checkPassword($password, $hash)
+    /**
+     * Checks weather a raw password and its hashed value match
+     *
+     * @param string $password
+     * @param string $hashed
+     * @return boolean
+     */
+    private function checkPassword($password, $hashed)
     {
-        return password_verify($password, $hash);
+        return password_verify($password, $hashed);
     }
 
+    /**
+     * Hashes a password
+     * 
+     * @param string $password
+     * @return string
+     */
     private function hashPassword($password)
     {
         return password_hash($password, PASSWORD_DEFAULT);
     }
 
+    /**
+     * Set session with authenticated user details
+     * 
+     * @param User $user
+     */
     private function setSession(User $user)
     {
         $_SESSION['auth'] = [
-            'user' => $user->id,
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+            ],
             'start' => date('Y-m-d H:i:s')
         ];
     }
 
     /**
-     * Destroy the session
+     * Destroys the session
      */
     public function logout()
     {
@@ -79,8 +109,20 @@ class Authentication
         $this->authenticated = false;
     }
 
+    /**
+     * Returns authentication errors
+     * @return array
+     */
     public function getErrors()
     {
         return $this->errors;
+    }
+
+    public function getAuthenticatedUser()
+    {
+        $return = [];
+        if ($this->isAuthenticated()) {
+            return $_SESSION['auth']['user'];
+        }
     }
 }
